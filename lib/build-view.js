noseglid/atom-build

commit 392d556f841ba2601be640e6059abbda7c3163cf

don't attach view on package activation

lib/build-view.js
@@ -18,7 +18,7 @@ module.exports = (function() {
     this.titleLoopIndex = 0;
     this.a2h = new Convert();
     this.monocle = false;
-    this.attach();
+    // this.attach();
 
     atom.config.observe('build.panelVisibility', this.visibleFromConfig.bind(this));
     atom.config.observe('build.monocleHeight', this.heightFromConfig.bind(this));
@@ -95,10 +95,10 @@ module.exports = (function() {
           this.detach();
         }
         break;
-
-      case 'Keep Visible':
-        this.attach();
-        break;
+      //
+      // case 'Keep Visible':
+      //   this.attach();
+      //   break;
     }
   };
 

spec/build-spec.js
@@ -65,7 +65,7 @@ describe('Build', function() {
     it('should not leave multiple panels behind', function() {
       expect(workspaceElement.querySelector('.build')).not.toExist();
 
-      atom.config.set('build.panelVisibility', 'Keep Visible');
+      atom.commands.dispatch(workspaceElement, 'build:toggle-panel');
 
       fs.writeFileSync(directory + 'Makefile', fs.readFileSync(goodMakefile));
       atom.commands.dispatch(workspaceElement, 'build:trigger');

spec/build-visible-spec.js
@@ -45,8 +45,8 @@ describe('Visible', function() {
       });
     });
 
-    it('should show build window', function() {
-      expect(workspaceElement.querySelector('.build')).toExist();
+    it('should not show build window', function() {
+      expect(workspaceElement.querySelector('.build')).not.toExist();
     });
   });
 
@@ -60,10 +60,7 @@ describe('Visible', function() {
 
     describe('when build panel is toggled and it is visible', function() {
       beforeEach(function () {
-        atom.config.set('build.panelVisibility', 'Keep Visible');
-        waitsForPromise(function () {
-          return atom.packages.activatePackage('build');
-        });
+        atom.commands.dispatch(workspaceElement, 'build:toggle-panel');
       });
 
       it('should hide the build panel', function() {

commit a7ddcc9eecd74d9146d271ec96f11790f82f213a

BuildView: set configured height on attach
Regression introduced in 392d556.
Set the configured height percent when attaching.

Fixes #118

lib/build-view.js
@@ -61,6 +61,7 @@ module.exports = (function() {
     }
     this.panel = atom.workspace.addBottomPanel({ item: this });
     this.height = this.output.offset().top + this.output.height();
+    this.heightFromConfig();
   };
 
   BuildView.prototype.detach = function(force) {
@@ -78,7 +79,7 @@ module.exports = (function() {
     return !!this.panel;
   };
 
-  BuildView.prototype.heightFromConfig = function(val) {
+  BuildView.prototype.heightFromConfig = function() {
     if (this.monocle) {
       this.setHeightPercent(atom.config.get('build.monocleHeight'));
     } else {