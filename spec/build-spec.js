19513d3e948faacc9724ab1298e795deae04c9a3
Alexander Olsson <noseglid@gmail.com>
2015-08-29 23:56:17 +0200

    Tools: Removed references to npm/apm

--- spec/build-spec.js
+++ spec/build-spec.js
@@ -1,15 +1,11 @@
 'use babel';
 'use strict';
 
-var _ = require('lodash');
 var fs = require('fs-extra');
 var temp = require('temp');
 var specHelpers = require('atom-build-spec-helpers');
 
 describe('Build', function() {
-  var goodNodefile = __dirname + '/fixture/package.json.node';
-  var goodAtomfile = __dirname + '/fixture/package.json.atom';
-  var badPackageJsonfile = __dirname + '/fixture/package.json.noengine';
   var goodAtomBuildfile = __dirname + '/fixture/.atom-build.json';
   var shellAtomBuildfile = __dirname + '/fixture/.atom-build.shell.json';
   var replaceAtomBuildFile = __dirname + '/fixture/.atom-build.replace.json';
@@ -146,99 +142,6 @@ describe('Build', function() {
 
       runs(function() {
         expect(workspaceElement.querySelectorAll('.bottom.tool-panel.panel-bottom').length).toBe(1);
-      });
-    });
-  });
-
-  describe('when build is triggered with package.json file', function() {
-    it('should show the build window if it is node engine', function() {
-      expect(workspaceElement.querySelector('.build')).not.toExist();
-
-      fs.writeFileSync(directory + 'package.json', fs.readFileSync(goodNodefile));
-      atom.commands.dispatch(workspaceElement, 'build:trigger');
-
-      waitsFor(function() {
-        return workspaceElement.querySelector('.build .title') &&
-          workspaceElement.querySelector('.build .title').classList.contains('success');
-      });
-
-      runs(function() {
-        expect(workspaceElement.querySelector('.build')).toExist();
-        expect(workspaceElement.querySelector('.build .output').textContent).toMatch(/^Executing: npm/);
-      });
-    });
-
-    it('should show the build window if it is atom engine', function() {
-      if (process.env.TRAVIS) {
-        return;
-      }
-
-      expect(workspaceElement.querySelector('.build')).not.toExist();
-
-      fs.writeFileSync(directory + 'package.json', fs.readFileSync(goodAtomfile));
-      atom.commands.dispatch(workspaceElement, 'build:trigger');
-
-      waitsFor(function() {
-        return workspaceElement.querySelector('.build .title') &&
-          workspaceElement.querySelector('.build .title').classList.contains('success');
-      }, 'build to be successful', 10000);
-
-      runs(function() {
-        expect(workspaceElement.querySelector('.build')).toExist();
-        expect(workspaceElement.querySelector('.build .output').textContent).toMatch(/^Executing: apm/);
-      });
-    });
-
-    it('should not do anything if engines are not available in the file', function() {
-      expect(workspaceElement.querySelector('.build')).not.toExist();
-
-      fs.writeFileSync(directory + 'package.json', fs.readFileSync(badPackageJsonfile));
-      atom.commands.dispatch(workspaceElement, 'build:trigger');
-
-      waits(1000);
-
-      runs(function() {
-        expect(workspaceElement.querySelector('.build')).not.toExist();
-      });
-    });
-
-    it('should list scripts as build targets', function () {
-      expect(workspaceElement.querySelector('.build')).not.toExist();
-
-      fs.writeFileSync(directory + 'package.json', fs.readFileSync(goodNodefile));
-      runs(function () {
-        atom.commands.dispatch(workspaceElement, 'build:select-active-target');
-      });
-
-      waitsFor(function () {
-        return workspaceElement.querySelector('.select-list li.build-target');
-      });
-
-      runs(function () {
-        var targets = _.map(workspaceElement.querySelectorAll('.select-list li.build-target'), function (el) {
-          return el.textContent;
-        });
-        expect(targets).toEqual([ 'npm: default', 'npm: custom script' ]);
-      });
-    });
-
-    it('should list package.json files with engine atom scripts as run by NPM', function () {
-      expect(workspaceElement.querySelector('.build')).not.toExist();
-
-      fs.writeFileSync(directory + 'package.json', fs.readFileSync(goodAtomfile));
-      runs(function () {
-        atom.commands.dispatch(workspaceElement, 'build:select-active-target');
-      });
-
-      waitsFor(function () {
-        return workspaceElement.querySelector('.select-list li.build-target');
-      });
-
-      runs(function () {
-        var targets = _.map(workspaceElement.querySelectorAll('.select-list li.build-target'), function (el) {
-          return el.textContent;
-        });
-        expect(targets).toEqual([ 'apm: default', 'npm: custom script' ]);
       });
     });
   });
